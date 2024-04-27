cef6067cd369c6ff2f28011970ada80fb0f9387f
Captain-Slow2000 <112606090+Captain-Slow2000@users.noreply.github.com>
2024-04-27 16:37:35 -0700

    More fixes for LCD Manager

--- include/lcdManager.hpp
+++ include/lcdManager.hpp
@@ -18,22 +18,8 @@
 
 extern QueueHandle_t lightActions;
 
-int sendLights = LIGHT_SIGNAL_OFF;
-
 static char returnString[50];
 static char otherReturnString[50];
-
-struct tm ntptime;
-
-volatile bool button_Pressed = false; // used as a flag for switch debouncing
-volatile bool piezo_Beep = false; // used in isr to toggle piezobuzzer on/off 
-
-const long gmtOffset_sec = -28800; // get time for PDT time zone 
-const int daylightOffset_sec = 3600; // account for daylight savings
-const char* ntpServer = "pool.ntp.org"; // target server to get time from internet
-
-               // RS, E,  D4, D5, D6, D7
-LiquidCrystal lcd(13, 12, 14, 27, 26, 25);
 
 void getTime();
 void IRAM_ATTR ISR();
--- src/lcdManager.cpp
+++ src/lcdManager.cpp
@@ -16,6 +16,21 @@
 
 // interrupt is triggered by S1 pushbutton on the
 // Vandaluino Shield, toggles piezobuzzer on/off
+
+               // RS, E,  D4, D5, D6, D7
+LiquidCrystal lcd(13, 12, 14, 27, 26, 25);
+
+struct tm ntptime;
+
+int sendLights = LIGHT_SIGNAL_OFF;
+
+volatile bool button_Pressed = false; // used as a flag for switch debouncing
+volatile bool piezo_Beep = false; // used in isr to toggle piezobuzzer on/off 
+
+const long gmtOffset_sec = -28800; // get time for PDT time zone 
+const int daylightOffset_sec = 3600; // account for daylight savings
+const char* ntpServer = "pool.ntp.org"; // target server to get time from internet
+
 void IRAM_ATTR ISR(){
   // debounce logic
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
