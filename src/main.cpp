36917dfc53b9aae91a28a4746430c821760a3035
CarsonRueber <carsonrueber@gmail.com>
2024-04-28 16:37:21 -0700

    Update main.cpp to remove testing wifi

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ const char* ssidAVG = "GoVandals!"; // name of Wi-Fi network
 void setup() {
 	Serial.begin(MONITOR_SPEED);
 	gui.init();
-        
+				
 	pinMode(GPIO_INTERRUPT, INPUT_PULLDOWN);
 	pinMode(ALARM_PIN, OUTPUT);
 
@@ -25,33 +25,33 @@ void setup() {
 
 	xTaskCreate(vLCDTask, "LCD", 2048, NULL, 5, NULL);
 
-        lightActions = xQueueCreate(16, sizeof(int));
-        xTaskCreate(task_lightManager, "Light Manager", 2048, NULL, 5, NULL);
-        
-        Wire.begin();
-        //first call to the function initializes the sensor
-        double a, b;
-        getTempHumid(a, b);
+	lightActions = xQueueCreate(16, sizeof(int));
+	xTaskCreate(task_lightManager, "Light Manager", 2048, NULL, 5, NULL);
+	
+	Wire.begin();
+	//first call to the function initializes the sensor
+	double a, b;
+	getTempHumid(a, b);
 
-        newAlarms = xQueueCreate(16, sizeof(alarmDefinition));
-        listAlarms = xQueueCreate((MAX_ALARMS * 2), sizeof(alarmDefinition));
-        shouldListAlarms = xSemaphoreCreateBinary();
-        xTaskCreate(task_alarmScheduler, "Alarm Scheduler", 4096, NULL, 5, NULL);
+	newAlarms = xQueueCreate(16, sizeof(alarmDefinition));
+    listAlarms = xQueueCreate((MAX_ALARMS * 2), sizeof(alarmDefinition));
+    shouldListAlarms = xSemaphoreCreateBinary();
+	xTaskCreate(task_alarmScheduler, "Alarm Scheduler", 4096, NULL, 5, NULL);
 
-        WiFi.setHostname("AVG_ESP32"); // set a host name for the ESP32
-        delay(1000);
-        WiFi.mode(WIFI_AP_STA); // set mode of WiFi to Station, connect to a pre-existing network
-        delay(1000);
+	//WiFi.setHostname("AVG_ESP32"); // set a host name for the ESP32
+	//delay(1000);
+	//WiFi.mode(WIFI_AP_STA); // set mode of WiFi to Station, connect to a pre-existing network
+	//delay(1000);
 
-        WiFi.begin(ssidAVG, passwordAVG); // connect to specified network and enter password
-        delay(1000);
-        //status = WiFi.status(); // get current status of WiFi connection
+	//WiFi.begin(ssidAVG, passwordAVG); // connect to specified network and enter password
+	//delay(1000);
+	//status = WiFi.status(); // get current status of WiFi connection
 
-        while(status != WL_CONNECTED){
-          printf("Connecting...\n");
-          status = WiFi.begin(ssidAVG, passwordAVG);
-          delay(1000);
-        }
+	//while(status != WL_CONNECTED){
+	//	printf("Connecting...\n");
+	//	status = WiFi.begin(ssidAVG, passwordAVG);
+	//	delay(1000);
+	//}
 
 }
 
